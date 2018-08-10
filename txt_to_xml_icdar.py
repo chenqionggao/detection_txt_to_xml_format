67bca0cace71a310edff06f12b22d155a1ee89b4
chenqionggao <chenqionggao@gmail.com>
2018-08-10 20:25:07 +0800

    Add files via upload

---
+++ txt_to_xml_icdar.py
@@ -0,0 +1,58 @@
+
+#! /usr/bin/python
+
+import os, sys
+import glob
+from PIL import Image
+#img_999.jpg img_999.txt
+src_img_dir = "train/"
+src_txt_dir = "train/"
+
+img_Lists = glob.glob(src_img_dir + '/*.jpg')
+
+img_basenames = [] # e.g. img_100.jpg
+for item in img_Lists:
+    img_basenames.append(os.path.basename(item))
+
+img_names = [] # e.g. img_100 jpg
+for item in img_basenames:
+    temp1, temp2 = os.path.splitext(item)
+    img_names.append(temp1)
+
+for img in img_names:
+    im = Image.open((src_img_dir + '/' + img + '.jpg'))
+    width, height = im.size
+
+    # open the crospronding txt file
+    gt = open(src_txt_dir + '/' + img + '.txt').read().splitlines()
+
+    # write in xml file
+    os.mknod(src_txt_dir + '/' + img + '.xml')
+    xml_file = open((src_txt_dir + '/' + img + '.xml'), 'w')
+    xml_file.write('<annotation>\n')
+    xml_file.write('    <folder>VOC2007</folder>\n')
+    xml_file.write('    <filename>' + str(img) + '.jpg' + '</filename>\n')
+    xml_file.write('    <size>\n')
+    xml_file.write('        <width>' + str(width) + '</width>\n')
+    xml_file.write('        <height>' + str(height) + '</height>\n')
+    xml_file.write('        <depth>3</depth>\n')
+    xml_file.write('    </size>\n')
+
+    # write the region of text on xml file
+    for img_each_label in gt:
+	spt = img_each_label.split(',')
+	if(spt[1] == spt[3]):
+            xml_file.write('    <object>\n')
+            xml_file.write('        <name>text</name>\n')
+            xml_file.write('        <pose>Unspecified</pose>\n')
+            xml_file.write('        <truncated>0</truncated>\n')
+            xml_file.write('        <difficult>0</difficult>\n')
+            xml_file.write('        <bndbox>\n')
+            xml_file.write('            <xmin>' + str(spt[0]) + '</xmin>\n')
+            xml_file.write('            <ymin>' + str(spt[1]) + '</ymin>\n')
+            xml_file.write('            <xmax>' + str(spt[4]) + '</xmax>\n')
+            xml_file.write('            <ymax>' + str(spt[5]) + '</ymax>\n')
+            xml_file.write('        </bndbox>\n')
+            xml_file.write('    </object>\n')
+
+    xml_file.write('</annotation>')
